81c3c82103eb19fc5de59d5e342527f114dd4b1e
DAAC-A00 <wnsdudrkwh@gmail.com>
2024-03-24 01:27:25 +0900

    counterProvider로 counter 구현된 homeScreen으로 수정

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,44 +1,28 @@
 // home_screen.dart
 
+// home_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// Providers are declared globally and specify how to create a state
-final counterProvider = StateProvider((ref) => 0);
+import 'counter_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
+
     return Scaffold(
-        appBar: AppBar(title: const Text('Counter example')),
-        body: Center(
-          // Consumer is a builder widget that allows you to read providers.
-          child: Consumer(
-            builder: (context, ref, _) {
-              final count = ref.watch(counterProvider);
-              return Text('$count');
-            },
-          ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              // The read method is a utility to read a provider without listening to it
-              onPressed: () => ref.read(counterProvider.notifier).state++,
-              child: const Icon(Icons.add),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            FloatingActionButton(
-              // The read method is a utility to read a provider without listening to it
-              onPressed: () => ref.read(counterProvider.notifier).state--,
-              child: const Icon(Icons.remove),
-            ),
-          ],
-        ));
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Text('Counter: $count',
+            style: Theme.of(context).textTheme.headlineMedium),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(counterProvider.notifier).state++,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
